2805dc32c03e995832b015a404712bc029a3eceb
burcakulug <burcak.ulug@feisystems.com>
2015-10-01 15:13:36 -0400

    Implement provider lookup controller
    #7058: Create the search provider UI that will interface with PLS API

--- client/app/provider/controllers/providerController.js
+++ client/app/provider/controllers/providerController.js
@@ -7,36 +7,69 @@
 angular.module("app.providerModule", ['ui.router', 'app.providerService', 'app.providerDirectives'])
 
     .config(['$stateProvider', function ($stateProvider) {
-            $stateProvider
-                .state('provider', {
-                    abstract: true,
-                    url: '/provider',
-                    data: { pageTitle: 'Provider' },
-                    templateUrl: 'app/common/content.tpl.html'
-                })
-                .state('provider.list', {
-                    url: '/list',
-                    params: { scrollTo : null, expand: null},
-                    data: { pageTitle: 'Provider List' },
-                    templateUrl: 'app/provider/tmpl/provider-list.tpl.html',
-                    controller: 'ProviderListController'
-                })
-                .state('provider.lookup', {
-                    url: '/lookup',
-                    params: { scrollTo : null, expand: null},
-                    data: { pageTitle: 'Provider lookup' },
-                    templateUrl: 'app/provider/tmpl/provider-lookup.tpl.html',
-                    controller: 'ProviderLookupController'
-                });
-         }
+        $stateProvider
+            .state('provider', {
+                abstract: true,
+                url: '/provider',
+                data: {pageTitle: 'Provider'},
+                templateUrl: 'app/common/content.tpl.html'
+            })
+            .state('provider.list', {
+                url: '/list',
+                params: {scrollTo: null, expand: null},
+                data: {pageTitle: 'Provider List'},
+                templateUrl: 'app/provider/tmpl/provider-list.tpl.html',
+                controller: 'ProviderListController'
+            })
+            .state('provider.lookup', {
+                url: '/lookup',
+                params: {scrollTo: null, expand: null},
+                data: {pageTitle: 'Provider lookup'},
+                templateUrl: 'app/provider/tmpl/provider-lookup.tpl.html',
+                controller: 'ProviderLookupController'
+            });
+    }
     ])
 
-    .controller('ProviderListController', ['$scope','ProviderService',
+    .controller('ProviderListController', ['$scope', 'ProviderService',
         function ($scope, ProviderService) {
 
-    }])
+        }])
 
-    .controller('ProviderLookupController', ['$scope','ProviderService',
+    .controller('ProviderLookupController', ['$scope', 'ProviderService',
         function ($scope, ProviderService) {
 
-    }]);
+            $scope.showSearch = true;
+
+            $scope.lookupProvider = function (pageNumber) {
+                $scope.showSearch = false;
+                var queryParameters = $scope.plsQueryParameters;
+                queryParameters.phone = queryParameters.phone1 + queryParameters.phone2 + queryParameters.phone3;
+                console.log(queryParameters);
+                ProviderService.getProviders(queryParameters).get({pageNumber: pageNumber},
+                    function (response) {
+                        console.log("SUCCESS:");
+                        console.log(response);
+                        $scope.providerLookupResult = response;
+                        console.log($scope.paginationSummary);
+                    },
+                    function (response) {
+                        console.log("ERROR:");
+                        console.log(response);
+                    }
+                );
+            };
+
+            $scope.getTotalPageNumber = function (totalPages) {
+                return new Array(totalPages);
+            };
+
+            $scope.paginationSummary = function () {
+                var rangeStart = ($scope.providerLookupResult.currentPage * $scope.providerLookupResult.itemsPerPage + 1);
+                var rangeEnd = (Math.min(($scope.providerLookupResult.totalNumberOfProviders), ($scope.providerLookupResult.currentPage * $scope.providerLookupResult.itemsPerPage + $scope.providerLookupResult.itemsPerPage)));
+                var total = ($scope.providerLookupResult.totalNumberOfProviders);
+                var summary = 'Showing '.concat(rangeStart, ' to ', rangeEnd, ' of ', total, ' entries');
+                return summary;
+            };
+
+        }]);
