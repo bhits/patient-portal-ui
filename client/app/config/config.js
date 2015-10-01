a424aadd59ce78796fef0b5f00e0cd67e84f1c27
burcakulug <burcak.ulug@feisystems.com>
2015-10-01 15:09:46 -0400

    Add endpoint URL configuration for PLS REST api
    #7058: Create the search provider UI that will interface with PLS API

--- client/app/config/config.js
+++ client/app/config/config.js
@@ -2,6 +2,6 @@
 
  angular.module('app.config', [])
 
-.constant('ENVService', {name:'Development',version:'0.0.1',stsBaseUri:'https://sts-dev.feisystems.com/identity/tokens',apiBaseUrl:'https://testbed-api-dev.feisystems.com'})
+.constant('ENVService', {name:'Development',version:'0.0.1',stsBaseUri:'https://sts-dev.feisystems.com/identity/tokens',apiBaseUrl:'https://testbed-api-dev.feisystems.com',plsApiBaseUrl:'http://localhost:8080/provider-web/providers'})
 
 ;
