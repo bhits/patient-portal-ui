41c72658528eb72b4e633b4d7d7ccfe0abd70727
burcakulug <burcak.ulug@feisystems.com>
2015-10-05 13:58:42 -0400

    Fix provider name and address rendering issues on provider search results
    #7058: Create the search provider UI that will interface with PLS API

--- client/app/provider/directives/providerDirectives.js
+++ client/app/provider/directives/providerDirectives.js
@@ -6,27 +6,61 @@
 
 angular.module("app.providerDirectives", [])
 
-    .directive('providerLookupResult', ['$location', '$anchorScroll',function ($location, $anchorScroll) {
+    .directive('providerLookupResult', ['$location', '$anchorScroll', function ($location, $anchorScroll) {
         return {
             restrict: 'E',
             scope: {
                 providerLookupResult: '='
             },
             templateUrl: 'app/provider/tmpl/provider-lookup-result.tpl.html',
-            controller: ['$scope', function($scope){
+            controller: ['$scope', function ($scope) {
                 $location.hash('provider_lookup_result');
                 $anchorScroll();
+
+                $scope.getProviderName = function (provider) {
+                    var providerName = '';
+                    if (provider && provider.entityType === 'Organization') {
+                        providerName = provider.providerOrganizationName;
+                    } else if (provider && provider.entityType === 'Individual') {
+                        providerName = provider.providerLastName + ', ' + provider.providerFirstName + (provider.providerMiddleName ? ' ' + provider.providerMiddleName : '');
+                    }
+                    return providerName;
+                };
+
+                $scope.formatPhoneNumber = function (phoneNumber) {
+                    return phoneNumber.slice(0, 3) + "-" + phoneNumber.slice(3, 6) + "-" + phoneNumber.slice(6, 10) + (phoneNumber.length > 10 ? '-' + phoneNumber.slice(10) : '');
+                };
+
+                $scope.getProviderAddressAsArray = function (provider) {
+                    function formatZipCode(zipCode) {
+                        var formattedZipCode = zipCode;
+                        if (typeof zipCode === 'string' && zipCode.length > 5) {
+                            formattedZipCode = zipCode.slice(0, 5) + "-" + zipCode.slice(5);
+                        }
+                        return formattedZipCode;
+                    }
+
+                    var providerAddressArray = [provider.providerFirstLineBusinessPracticeLocationAddress,
+                        provider.providerSecondLineBusinessPracticeLocationAddress,
+                        provider.providerBusinessPracticeLocationAddressCityName,
+                        provider.providerBusinessPracticeLocationAddressStateName,
+                        formatZipCode(provider.providerBusinessPracticeLocationAddressPostalCode)].filter(function (element) {
+                            return typeof element === 'string' && element.length > 0;
+                        });
+                    return providerAddressArray.join(", ");
+
+                };
             }]
         };
     }])
 
-    .directive('providerLookupSearch',function(){
+    .directive('providerLookupSearch', function () {
         return {
             restrict: 'E',
             scope: {},
             templateUrl: 'app/provider/tmpl/provider-lookup-search.tpl.html',
-            controller: ['$scope','$location','ProviderService',
-                function ($scope, $location,ProviderService) {
+            controller: ['$scope', '$location', 'ProviderService',
+                function ($scope, $location, ProviderService) {
 
                     $scope.showSearch = true;
 
@@ -62,7 +96,6 @@ angular.module("app.providerDirectives", [])
                         var summary = 'Showing '.concat(rangeStart, ' to ', rangeEnd, ' of ', total, ' entries');
                         return summary;
                     };
-
                 }]
         };
 
