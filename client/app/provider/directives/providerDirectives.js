eabb9b751c2a32f5de66fe7979f5729da14cba52
burcakulug <burcak.ulug@feisystems.com>
2015-10-05 12:18:10 -0400

    Implement directives for provider lookup search and search results
    #7058: Create the search provider UI that will interface with PLS API

--- client/app/provider/directives/providerDirectives.js
+++ client/app/provider/directives/providerDirectives.js
@@ -4,4 +4,68 @@
 
 'use strict';
 
-angular.module("app.providerDirectives", []);
+angular.module("app.providerDirectives", [])
+
+    .directive('providerLookupResult', ['$location', '$anchorScroll',function ($location, $anchorScroll) {
+        return {
+            restrict: 'E',
+            scope: {
+                providerLookupResult: '='
+            },
+            templateUrl: 'app/provider/tmpl/provider-lookup-result.tpl.html',
+            controller: ['$scope', function($scope){
+                $location.hash('provider_lookup_result');
+                $anchorScroll();
+            }]
+        };
+    }])
+
+    .directive('providerLookupSearch',function(){
+        return {
+            restrict: 'E',
+            scope: {},
+            templateUrl: 'app/provider/tmpl/provider-lookup-search.tpl.html',
+            controller: ['$scope','$location','ProviderService',
+                function ($scope, $location,ProviderService) {
+
+                    $scope.showSearch = true;
+
+                    $scope.lookupProvider = function (pageNumber) {
+                        $location.hash('');
+                        delete $scope.providerLookupResult;
+                        console.log($scope);
+                        $scope.showSearch = false;
+                        var queryParameters = $scope.plsQueryParameters;
+                        console.log(queryParameters);
+                        ProviderService.lookupProviders(queryParameters).get({pageNumber: pageNumber},
+                            function (response) {
+                                console.log("SUCCESS:");
+                                console.log(response);
+                                $scope.providerLookupResult = response;
+                                console.log($scope.paginationSummary);
+                            },
+                            function (response) {
+                                console.log("ERROR:");
+                                console.log(response);
+                            }
+                        );
+                    };
+
+                    $scope.getTotalPageNumber = function (totalPages) {
+                        return new Array(totalPages);
+                    };
+
+                    $scope.paginationSummary = function () {
+                        var rangeStart = ($scope.providerLookupResult.currentPage * $scope.providerLookupResult.itemsPerPage + 1);
+                        var rangeEnd = (Math.min(($scope.providerLookupResult.totalNumberOfProviders), ($scope.providerLookupResult.currentPage * $scope.providerLookupResult.itemsPerPage + $scope.providerLookupResult.itemsPerPage)));
+                        var total = ($scope.providerLookupResult.totalNumberOfProviders);
+                        var summary = 'Showing '.concat(rangeStart, ' to ', rangeEnd, ' of ', total, ' entries');
+                        return summary;
+                    };
+
+                }]
+        };
+
+    })
+
+;
