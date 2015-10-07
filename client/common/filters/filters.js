8cfee995a441e1ce08e196fd1c4387c0f7c9070b
tomson ngassa <tomson.ngassa@feisystems.com>
2015-10-07 17:06:06 -0400

    Add configuration for the pcm api
    #7057 - Create manage providers UI that interfaces with Patient Consent Management API

--- client/common/filters/filters.js
+++ client/common/filters/filters.js
@@ -1,3 +1,56 @@
-﻿'use strict';
+﻿
+(function(){
+    'use strict';
+    /**
+     * The address filter
+     *
+     * @returns {String} - The filter address strign
+     *
+     * @constructor
+     */
+    function AddressFilter (){
 
-//angular.module('app.filtersModule', []);
+        return function(provider){
+            var addressStr = "";
+            if(angular.isDefined(provider.firstLinePracticeLocationAddress)){
+                addressStr += provider.firstLinePracticeLocationAddress;
+            }
+            if(angular.isDefined(provider.practiceLocationAddressCityName)){
+                addressStr += ", " +provider.practiceLocationAddressCityName;
+            }
+            if(angular.isDefined(provider.practiceLocationAddressStateName)){
+                addressStr += ", " + provider.practiceLocationAddressStateName;
+            }
+            if(angular.isDefined(provider.practiceLocationAddressPostalCode)){
+                addressStr += ", " + provider.practiceLocationAddressPostalCode;
+            }
+
+            return addressStr;
+        };
+    }
+
+    /**
+     * Formats the individual provider first and last name or the organization name
+     *
+     * @returns {String} - The individual provider first and last name or the organization name
+     *
+     */
+    function NameOrFacilityFilter(){
+        return function(provider){
+            if(angular.equals(provider.entityType, 'Individual')){
+                return provider.lastName + "," + provider.firstName;
+            }else if(angular.equals(provider.entityType, 'Organization')){
+                return provider.orgName;
+            }
+        };
+    }
+
+    /**
+     * The generalize filter for the application
+     *
+     */
+    angular.module('app.filtersModule', [])
+        .filter('address', AddressFilter)
+        .filter('nameorfacility', NameOrFacilityFilter);
+})();
+
