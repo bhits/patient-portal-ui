1e06afbaadad46b091b5c9adeb6ce1ff0eca5b02
tomson ngassa <tomson.ngassa@feisystems.com>
2015-10-09 17:29:40 -0400

    Unit test the address and name or facility filters
    #7057 - Create manage providers UI that interfaces with Patient Consent Management AP

--- client/common/filters/filters.spec.js
+++ client/common/filters/filters.spec.js
@@ -28,3 +28,37 @@ xdescribe('app.filtersModule ', function(){
 
     });
 });
+
+describe('app.filter ', function () {
+
+    var address, nameorfacility;
+
+    beforeEach(module('app.filtersModule'));
+
+    beforeEach(inject(function (_addressFilter_, _nameorfacilityFilter_) {
+        address = _addressFilter_;
+        nameorfacility = _nameorfacilityFilter_;
+    }));
+
+    it('should filter address ', function () {
+
+        var provider = {firstLinePracticeLocationAddress: "7175 Columbia Gateway", practiceLocationAddressCityName: "Columbia", practiceLocationAddressStateName: "MD", practiceLocationAddressPostalCode: "21044"};
+        expect(address(provider)).toEqual("7175 Columbia Gateway, Columbia, MD, 21044");
+
+        var provider1 = {firstLinePracticeLocationAddress: "7175 Columbia Gateway", practiceLocationAddressCityName: "", practiceLocationAddressStateName: "MD", practiceLocationAddressPostalCode: "21044"};
+        expect(address(provider1)).toEqual("7175 Columbia Gateway, , MD, 21044");
+
+        var provider2 = {firstLinePracticeLocationAddress: "", practiceLocationAddressCityName: "", practiceLocationAddressStateName: "", practiceLocationAddressPostalCode: ""};
+        expect(address(provider2)).toEqual(", , , ");
+    });
+
+    it('should return first name and last name for Individual provider or facility name for orgnization provider', function () {
+
+        var provider = {firstName: "firstName", lastName: "lastName", entityType: "Individual"};
+        expect(nameorfacility(provider)).toEqual("lastName,firstName");
+
+        var provider1 = {orgName: "orgName", entityType: "Organization"};
+        expect(nameorfacility(provider1)).toEqual("orgName");
+    });
+
+});
