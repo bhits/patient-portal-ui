95570ae8b6218a04a258169bfba5f80b9c31a224
tomson ngassa <tomson.ngassa@feisystems.com>
2015-10-13 09:35:36 -0400

    Move the provider address and the name of facility filters in the provider filter
    #7057 - Create manage providers UI that interfaces with Patient Consent Management AP

--- client/common/filters/filters.js
+++ client/common/filters/filters.js
@@ -1,48 +1,5 @@
 ﻿(function () {
     'use strict';
-    /**
-     * The address filter
-     *
-     * @returns {String} - The filter address strign
-     *
-     * @constructor
-     */
-    function AddressFilter() {
-
-        return function (provider) {
-            var addressStr = "";
-            if (angular.isDefined(provider.firstLinePracticeLocationAddress)) {
-                addressStr += provider.firstLinePracticeLocationAddress;
-            }
-            if (angular.isDefined(provider.practiceLocationAddressCityName)) {
-                addressStr += ", " + provider.practiceLocationAddressCityName;
-            }
-            if (angular.isDefined(provider.practiceLocationAddressStateName)) {
-                addressStr += ", " + provider.practiceLocationAddressStateName;
-            }
-            if (angular.isDefined(provider.practiceLocationAddressPostalCode)) {
-                addressStr += ", " + provider.practiceLocationAddressPostalCode;
-            }
-
-            return addressStr;
-        };
-    }
-
-    /**
-     * Formats the individual provider first and last name or the organization name
-     *
-     * @returns {String} - The individual provider first and last name or the organization name
-     *
-     */
-    function NameOrFacilityFilter() {
-        return function (provider) {
-            if (angular.equals(provider.entityType, 'Individual')) {
-                return provider.lastName + "," + provider.firstName;
-            } else if (angular.equals(provider.entityType, 'Organization')) {
-                return provider.orgName;
-            }
-        };
-    }
 
     /**
      * Filter to format phone number
@@ -77,8 +34,6 @@
      *
      */
     angular.module('app.filtersModule', [])
-        .filter('address', AddressFilter)
-        .filter('nameorfacility', NameOrFacilityFilter)
         .filter('phone', PhoneNumberFilter)
         .filter('zip', ZipFilter);
 })();
