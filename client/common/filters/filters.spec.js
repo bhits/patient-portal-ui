3a7f3bdd306c1f82d79d8630d037446aa6cd5d3d
burcakulug <burcak.ulug@feisystems.com>
2015-10-14 15:47:47 -0400

    Refactor unit tests for common filters
    #7058: Create the search provider UI that will interface with PLS API

--- client/common/filters/filters.spec.js
+++ client/common/filters/filters.spec.js
@@ -34,112 +34,58 @@ describe('app.filtersModule ', function () {
 });
 
 describe('app.filtersModule', function () {
-    var hasString, utilityService;
+    var hasString;
+    var zip;
+    var phone;
+    var utilityService;
 
     beforeEach(module('app.filtersModule'));
 
-    beforeEach(inject(function (_hasStringFilter_, _utilityService_) {
+    beforeEach(inject(function (_hasStringFilter_,_zipFilter_, _phoneFilter_, _utilityService_) {
         hasString = _hasStringFilter_;
+        zip = _zipFilter_;
+        phone = _phoneFilter_;
         utilityService = _utilityService_;
     }));
 
-    it('should evaluate hasString as undefined when input is empty string', function () {
-        spyOn(utilityService, 'hasString').andReturn("aaa12345bbb");
-        expect(hasString("aaa12345bbb")).toEqual("aaa12345bbb");
-        expect(utilityService.hasString).toHaveBeenCalledWith('aaa12345bbb');
+    it('should evaluate hasString same as utilityService.hasString', function () {
+        // Arrange
+        var input ='inputValue';
+        var returnedValue = 'returnedValue';
+        spyOn(utilityService, 'hasString').andReturn(returnedValue);
+
+        // Act & Assert
+        expect(hasString(input)).toEqual(returnedValue);
+        expect(utilityService.hasString).toHaveBeenCalledWith(input);
     });
 
-
-
-
-    xit('should evaluate hasString as input string when input is not an empty string', function () {
+    it('should return zip as utilityService.formatZipCode', function () {
         // Arrange
-        //var input = 'someString', returnValue = input;
-        //result = 0;
-        //
-        //
-        //var mockUtilityService = {
-        //    hasString: function (str) {
-        //        if (input === str) {
-        //            return returnValue;
-        //        }
-        //    }
-        //};
-        //module(function ($provide) {
-        //    $provide.value('utilityService', mockUtilityService);
-        //});
-        //
-        //// Act
-        //result = $filter('hasString')(input);
-        //
-        //// Assert
-        //expect(result).toBeTruthy();
-        //expect(result).toEqual(input);
+        var input ='inputValue';
+        var returnedValue = 'returnedValue';
+        spyOn(utilityService, 'formatZipCode').andReturn(returnedValue);
+
+        // Act & Assert
+        expect(zip(input)).toEqual(returnedValue);
+        expect(utilityService.formatZipCode).toHaveBeenCalledWith(input);
     });
 
-    xit('should evaluate hasString as input string when input is not an empty string', function () {
+    it('should format phone number without extension', function () {
         // Arrange
-        var input, returnValue, result;
-        var mockUtilityService = {
-            hasString: function (str) {
-                if (input === str) {
-                    return returnValue;
-                }
-            }
-        };
-        module(function ($provide) {
-            $provide.value('utilityService', mockUtilityService);
-        });
+        var input ='1234567890';
+        var returnedValue = '123-456-7890';
 
-        // Act
-        result = $filter('hasString')(input);
-
-        // Assert
-        expect(result).toBeFalsy();
-        expect(result).toBeUndefined();
+        // Act & Assert
+        expect(phone(input)).toEqual(returnedValue);
     });
 
-    xit('should return zip as utilityService.formatZipCode returns', function () {
+    it('should format phone number with extension', function () {
         // Arrange
-        var input ='aaa12345bbb', returnValue='12345', result;
-        var mockUtilityService = {
-            formatZipCode: function (str) {
-                if (input === str) {
-                    return returnValue;
-                }
-            }
-        };
-        module(function ($provide) {
-            $provide.value('utilityService', mockUtilityService);
-        });
+        var input ='12345678901234';
+        var returnedValue = '123-456-7890-1234';
 
-        // Act
-        result = $filter('zip')(input);
-
-        // Assert
-        expect(result).toEqual(returnValue);
-    });
-
-    xit('should format phone number without extension', function () {
-        // Arrange
-        var input ='1234567890', returnValue='123-456-7890', result;
-
-        // Act
-        result = $filter('phone')(input);
-
-        // Assert
-        expect(result).toEqual(returnValue);
-    });
-
-    xit('should format phone number with extension', function () {
-        // Arrange
-        var input ='12345678901234', returnValue='123-456-7890-1234', result;
-
-        // Act
-        result = $filter('phone')(input);
-
-        // Assert
-        expect(result).toEqual(returnValue);
+        // Act & Assert
+        expect(phone(input)).toEqual(returnedValue);
     });
 
 })
