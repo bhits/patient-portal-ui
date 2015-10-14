3699125a3b1b3899f840472d93bb6eaeb8d7ec7c
burcakulug <burcak.ulug@feisystems.com>
2015-10-14 14:22:50 -0400

    Add unit tests for lookupProviders and isEmptyLookupResult provider service methods
    #7058: Create the search provider UI that will interface with PLS API

--- client/app/provider/services/providerServices.spec.js
+++ client/app/provider/services/providerServices.spec.js
@@ -3,39 +3,39 @@
  */
 'use strict';
 
-describe('app.providerService  ', function(){
+describe('app.providerService  ', function () {
     var module;
 
-    beforeEach(function() {
+    beforeEach(function () {
         module = angular.module("app.providerService");
     });
 
-    it("should be registered", function() {
+    it("should be registered", function () {
         expect(module).not.toEqual(null);
     });
 
-    describe("Dependencies:", function() {
+    describe("Dependencies:", function () {
 
         var dependencies;
 
-        var hasModule = function(m) {
+        var hasModule = function (m) {
             return dependencies.indexOf(m) >= 0;
         };
-        beforeEach(function() {
+        beforeEach(function () {
             dependencies = module.value('app.providerService').requires;
         });
 
-        it("should have ngResource as a dependency", function() {
+        it("should have ngResource as a dependency", function () {
             expect(hasModule('ngResource')).toEqual(true);
         });
 
-        it("should have app.config as a dependency", function() {
+        it("should have app.config as a dependency", function () {
             expect(hasModule('app.config')).toEqual(true);
         });
     });
 });
 
-describe('app.providerService ', function() {
+describe('app.providerService ', function () {
     var ProviderServices, $httpBackend;
 
     beforeEach(module('app.providerService'));
@@ -47,7 +47,7 @@ describe('app.providerService ', function() {
         });
     });
 
-    afterEach(function() {
+    afterEach(function () {
         $httpBackend.verifyNoOutstandingExpectation();
         $httpBackend.verifyNoOutstandingRequest();
     });
@@ -60,10 +60,11 @@ describe('app.providerService ', function() {
         $httpBackend.expectPOST('https://localhost:8443/pcm/patients/providers/11111').respond({status: 201});
         var status = ProviderServices.addProvider(
             11111,
-            function(data ){
+            function (data) {
                 status = data.status;
             },
-            function(error){});
+            function (error) {
+            });
         $httpBackend.flush();
         expect(status).toEqual(201);
     });
@@ -72,12 +73,132 @@ describe('app.providerService ', function() {
         $httpBackend.expectDELETE('https://localhost:8443/pcm/patients/providers/11111').respond({status: 201});
         var status = ProviderServices.deleteProvider(
             11111,
-            function(data ){
+            function (data) {
                 status = data.status;
             },
-            function(error){});
+            function (error) {
+            });
         $httpBackend.flush();
         expect(status).toEqual(201);
     });
 
+    it('should lookup providers with successful backend call', function () {
+        // Arrange
+        var statusCodeSuccess = 200;
+        var statusCodeError = 500;
+        var statusCodeNotSet = "STATUS_NOT_SET";
+        var statusCodeReturnedFromBackend = statusCodeSuccess;
+        var page = 10;
+        var plsQueryParameters = {
+            usstate: "usstateValue",
+            city: "cityValue",
+            zipcode: "zipcodeValue",
+            lastname: "lastnameValue",
+            firstname: "firstnameValue",
+            gender: "genderValue",
+            phone: "phoneValue",
+            facilityname: "facilitynameValue"
+        };
+
+        // Act
+        var statusWhenSuccess = statusCodeNotSet;
+        var statusWhenError = statusCodeNotSet;
+        ProviderServices.lookupProviders(plsQueryParameters, page, function (response) {
+            statusWhenSuccess = response.status;
+        }, function (response) {
+            statusWhenError = response.status;
+        });
+
+        // Assert
+        $httpBackend.expectGET('http://localhost:8080/pls/providers/pageNumber/' + page + "/usstate/usstateValue/city/cityValue/zipcode/zipcodeValue/gender/genderValue/phone/phoneValue/firstname/firstnameValue/lastname/lastnameValue/facilityname/facilitynameValue").respond(statusCodeReturnedFromBackend, {status: statusCodeReturnedFromBackend});
+        $httpBackend.flush();
+        expect(statusWhenSuccess).toEqual(statusCodeSuccess);
+        expect(statusWhenError).toEqual(statusCodeNotSet);
+    });
+
+
+    it('should get error status when lookup provider backend call fails', function () {
+        // Arrange
+        var statusCodeSuccess = 200;
+        var statusCodeError = 500;
+        var statusCodeNotSet = "STATUS_NOT_SET";
+        var statusCodeReturnedFromBackend = statusCodeError;
+        var page = 10;
+        var plsQueryParameters = {
+            usstate: "usstateValue",
+            city: "cityValue",
+            zipcode: "zipcodeValue",
+            lastname: "lastnameValue",
+            firstname: "firstnameValue",
+            gender: "genderValue",
+            phone: "phoneValue",
+            facilityname: "facilitynameValue"
+        };
+
+        // Act
+        var statusWhenSuccess = statusCodeNotSet;
+        var statusWhenError = statusCodeNotSet;
+        ProviderServices.lookupProviders(plsQueryParameters, page, function (response) {
+            statusWhenSuccess = response.status;
+        }, function (response) {
+            statusWhenError = response.status;
+        });
+
+        // Assert
+        $httpBackend.expectGET('http://localhost:8080/pls/providers/pageNumber/' + page + "/usstate/usstateValue/city/cityValue/zipcode/zipcodeValue/gender/genderValue/phone/phoneValue/firstname/firstnameValue/lastname/lastnameValue/facilityname/facilitynameValue").respond(statusCodeReturnedFromBackend, {status: statusCodeReturnedFromBackend});
+        $httpBackend.flush();
+        expect(statusWhenSuccess).toEqual(statusCodeNotSet);
+        expect(statusWhenError).toEqual(statusCodeError);
+    });
+
+    it('should return true if providerLookupResult is undefined', function () {
+        // Arrange
+        var providerLookupResult;
+        var expectedResult = true;
+
+        // Act
+        var result = ProviderServices.isEmptyLookupResult(providerLookupResult);
+
+        // Assert
+        expect(result).toEqual(expectedResult);
+    });
+
+    it('should return true if providerLookupResult.providers is undefined', function () {
+        // Arrange
+        var providerLookupResult = {someProperty: 'somePropertyValue'};
+        var expectedResult = true;
+
+        // Act
+        var result = ProviderServices.isEmptyLookupResult(providerLookupResult);
+
+        // Assert
+        expect(result).toEqual(expectedResult);
+    });
+
+    it('should return true if providerLookupResult.providers is defined but empty', function () {
+        // Arrange
+        var providerLookupResult = {someProperty: 'somePropertyValue', providers: []};
+        var expectedResult = true;
+
+        // Act
+        var result = ProviderServices.isEmptyLookupResult(providerLookupResult);
+
+        // Assert
+        expect(result).toEqual(expectedResult);
+    });
+
+    it('should return false if providerLookupResult.providers has at least one element', function () {
+        // Arrange
+        var providerLookupResult = {
+            someProperty: 'somePropertyValue',
+            providers: [{someObjectPropert: 'someObjectPropertyValue'}]
+        };
+        var expectedResult = false;
+
+        // Act
+        var result = ProviderServices.isEmptyLookupResult(providerLookupResult);
+
+        // Assert
+        expect(result).toEqual(expectedResult);
+    });
 });
