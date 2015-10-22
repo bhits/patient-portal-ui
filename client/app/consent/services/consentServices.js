db698cc328199496bd7ae65a5a49b675382bfbe8
burcakulug <burcak.ulug@feisystems.com>
2015-10-22 16:48:14 -0400

    Implement listConsent service to return mock data
    #7175: Implement list consent UI

--- client/app/consent/services/consentServices.js
+++ client/app/consent/services/consentServices.js
@@ -19,6 +19,44 @@
             },
 
             updateConsent: function(consent, success, error) {
+            },
+
+            listConsent: function(patientId) {
+                var consentList = [{
+                    "id": "1",
+                    "toDiscloseName": ["VAN DONGEN, MONICA"],
+                    "isMadeToName": ["GRIMES, MICHAEL"],
+                    "doNotShareClinicalDocumentTypeCodes": [],
+                    "doNotShareClinicalDocumentSectionTypeCodes": ["Medications", "Allergies"],
+                    "doNotShareSensitivityPolicyCodes": ["Mental health information sensitivity", "HIV/AIDS information sensitivity"],
+                    "shareForPurposeOfUseCodes": ["Payment", "Emergency Treatment", "Healthcare Treatment"],
+                    "doNotShareClinicalConceptCodes": [],
+                    "consentStage": "CONSENT_SIGNED",
+                    "revokeStage": "REVOCATION_NOT_SUBMITTED",
+                    "consentStart": 1404446399000,
+                    "consentEnd": 1437537600000,
+                    "consentStartString": null,
+                    "consentEndString": null,
+                    "medicalInformationNotDisclosed": true
+                }, {
+                    "id": "2",
+                    "toDiscloseName": ["GRIMES, MICHAEL", "NEVAEH LLC", "CARLSON, GEORGE"],
+                    "isMadeToName": ["VAN DONGEN, MONICA", "LUQUIN, TERESA", "MASTER CARE, INC."],
+                    "doNotShareClinicalDocumentTypeCodes": [],
+                    "doNotShareClinicalDocumentSectionTypeCodes": [],
+                    "doNotShareSensitivityPolicyCodes": ["Mental health information sensitivity", "HIV/AIDS information sensitivity"],
+                    "shareForPurposeOfUseCodes": ["Payment", "Emergency Treatment", "Healthcare Treatment"],
+                    "doNotShareClinicalConceptCodes": [],
+                    "consentStage": "CONSENT_SIGNED",
+                    "revokeStage": "REVOCATION_NOT_SUBMITTED",
+                    "consentStart": 1404446399000,
+                    "consentEnd": 1437537600000,
+                    "consentStartString": null,
+                    "consentEndString": null,
+                    "medicalInformationNotDisclosed": true
+                }
+                ];
+                return consentList;
             }
         };
     }
