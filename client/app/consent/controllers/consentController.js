5675c1dd5cab531285878e8327d32810817bfbb1
tomson ngassa <tomson.ngassa@feisystems.com>
2015-11-05 12:41:18 -0500

    Remvoe the Revoke Consent  Controller reference
     #7178 - Implement create consent UI

--- client/app/consent/controllers/consentController.js
+++ client/app/consent/controllers/consentController.js
@@ -46,7 +46,6 @@
             'app.consentServices',
             'app.consentDirectives'
         ])
-        .config(ConsentConfig)
-        .controller('ConsentRevokeVm', ConsentRevokeController);
+        .config(ConsentConfig);
 
 })();
