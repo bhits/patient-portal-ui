129012d67bb28bd1412b0899bf3e9b0106cdff00
burcakulug <burcak.ulug@feisystems.com>
2015-11-18 10:00:27 -0500

    Add Spring Boot server application to wrap Angular UI app
    #7346: User (patient) can login to the patient portal to access the PHR API

---
+++ server/src/main/java/gov/samhsa/bhits/ppui/PPUIApplication.java
@@ -0,0 +1,12 @@
+package gov.samhsa.bhits.ppui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PPUIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PPUIApplication.class, args);
+    }
+}
