3f144977dd831f52e3375c84930178ad95601264
tomson ngassa <tomson.ngassa@feisystems.com>
2015-12-11 14:37:45 -0500

    Change annonimous function to named function in ConsentMedicalInformation directives.
     #7178 - Implement create consent UI

--- client/app/consent/directives/ppConsentMedicalInformation.directive.js
+++ client/app/consent/directives/ppConsentMedicalInformation.directive.js
@@ -20,7 +20,7 @@
                     },
                     bindToController: true,
                     controllerAs: 'consentMedicalInformationVm',
-                    controller: MedicalInformationController,
+                    controller: MedicalInformationController
                 };
 
                 return directive;
@@ -34,65 +34,74 @@
                 //Initiallizing the medical information model
                 Vm.selectedMedicalSections = consentService.getLookupEntities(Vm.medicalsections, Vm.ngModel.doNotShareClinicalDocumentSectionTypeCodes);
                 Vm.selectedSensitivityPolicies = consentService.getLookupEntities(Vm.sensitivitypolicies, Vm.ngModel.doNotShareSensitivityPolicyCodes);
+                Vm.hasException = hasException;
+                Vm.clearMedicalInfoData = clearMedicalInfoData;
+                Vm.openPrivacySettingsModal = openPrivacySettingsModal;
 
-                //Vm.ngModel = {doNotShareSensitivityPolicyCodes : [],doNotShareClinicalDocumentSectionTypeCodes: []};
-                Vm.hasException = function () {
+                function hasException() {
                     return ((Vm.selectedMedicalSections.length > 0 ) || (Vm.selectedSensitivityPolicies.length > 0) || (Vm.medicalInformation === 'B') );
-                };
+                }
 
-                Vm.clearMedicalInfoData = function () {
+                function clearMedicalInfoData () {
                     Vm.medicalInformation = 'A';
                     Vm.selectedMedicalSections = [];
                     Vm.selectedSensitivityPolicies = [];
-                };
+                }
 
                 function MedicalInformationModalController($scope, $modalInstance, data) {
                     $scope.mediactionSections = data.mediactionSections;
                     $scope.sensitivityPolicies = data.sensitivityPolicies;
-
                     $scope.consent = [];
-
                     $scope.consent.selectedMedicalSections = !angular.isDefined(Vm.selectedMedicalSections) ? [] : consentService.getCodes(Vm.selectedMedicalSections);
                     $scope.consent.selectedSensitivityPolicies = !angular.isDefined(Vm.selectedSensitivityPolicies) ? [] : consentService.getCodes(Vm.selectedSensitivityPolicies);
 
-                    $scope.$watch("Vm.selectedMedicalSections", function (arg) {
+                    $scope.$watch("Vm.selectedMedicalSections", watchMedicalSection);
+                    $scope.$watch("Vm.selectedSensitivityPolicies",watchSensitivityPolicies );
+
+                    $scope.selectAllMedicalSections = selectAllMedicalSections;
+                    $scope.deselectAllMedicalSections = deselectAllMedicalSections;
+                    $scope.selectAllSensitivityPolicies = selectAllSensitivityPolicies;
+                    $scope.deselectAllSensitivityPolicies = deselectAllSensitivityPolicies;
+                    $scope.ok = ok;
+                    $scope.cancel = cancel;
+
+                    function watchMedicalSection (arg) {
                         if (Vm.medicalInformation === 'A') {
                             $scope.consent.selectedSensitivityPolicies = [];
                         }
-                    });
+                    }
 
-                    $scope.$watch("Vm.selectedSensitivityPolicies", function (arg) {
+                    function watchSensitivityPolicies(arg) {
                         if (Vm.medicalInformation === 'A') {
                             $scope.consent.selectedSensitivityPolicies = [];
                         }
-                    });
+                    }
 
-
-                    $scope.selectAllMedicalSections = function () {
+                    function selectAllMedicalSections () {
                         $scope.consent.selectedMedicalSections = consentService.getCodes($scope.mediactionSections);
                         //TODO: This should be refactored to avoid directly referencing the 'medicalInformationModalObj' name, which is declared in the HTML markup but not in this directive.
                         $scope.medicalInfofmationModalObj.$setDirty();
-                    };
+                    }
 
-                    $scope.deselectAllMedicalSections = function () {
+                    function deselectAllMedicalSections () {
                         $scope.consent.selectedMedicalSections = [];
                         //TODO: This should be refactored to avoid directly referencing the 'medicalInformationModalObj' name, which is declared in the HTML markup but not in this directive.
                         $scope.medicalInfofmationModalObj.$setDirty();
-                    };
+                    }
 
-                    $scope.selectAllSensitivityPolicies = function () {
+                    function selectAllSensitivityPolicies () {
                         $scope.consent.selectedSensitivityPolicies = consentService.getCodes($scope.sensitivityPolicies);
                         //TODO: This should be refactored to avoid directly referencing the 'medicalInformationModalObj' name, which is declared in the HTML markup but not in this directive.
                         $scope.medicalInfofmationModalObj.$setDirty();
-                    };
+                    }
 
-                    $scope.deselectAllSensitivityPolicies = function () {
+                    function deselectAllSensitivityPolicies () {
                         $scope.consent.selectedSensitivityPolicies = [];
                         //TODO: This should be refactored to avoid directly referencing the 'medicalInformationModalObj' name, which is declared in the HTML markup but not in this directive.
                         $scope.medicalInfofmationModalObj.$setDirty();
-                    };
+                    }
 
-                    $scope.ok = function () {
+                    function ok() {
                         Vm.selectedMedicalSections = consentService.getEntitiesByCodes($scope.mediactionSections, $scope.consent.selectedMedicalSections);
                         Vm.selectedSensitivityPolicies = consentService.getEntitiesByCodes($scope.sensitivityPolicies, $scope.consent.selectedSensitivityPolicies);
                         Vm.ngModel = {
@@ -101,14 +110,14 @@
                         };
 
                         $modalInstance.close();
-                    };
+                    }
 
-                    $scope.cancel = function () {
+                    function cancel() {
                         $modalInstance.dismiss('cancel');
-                    };
+                    }
                 }
 
-                Vm.openPrivacySettingsModal = function () {
+                function openPrivacySettingsModal () {
                     var modalInstance = $modal.open({
                         templateUrl: 'app/consent/directives/consentMedicalInformationModal.tpl.html',
 
@@ -122,7 +131,7 @@
                         },
                         controller: MedicalInformationModalController
                     });
-                };
+                }
             }
 
 })();
