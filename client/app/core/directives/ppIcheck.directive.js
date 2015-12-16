0b2ccfd83e556ea80b90a8db1a17e264f0a59bba
tomson ngassa <tomson.ngassa@feisystems.com>
2015-12-16 09:51:21 -0500

    Apply Angular style guide to ppIcheck directive.
    #7178 - Implement create consent UI

--- client/app/core/directives/ppIcheck.directive.js
+++ client/app/core/directives/ppIcheck.directive.js
@@ -11,38 +11,49 @@
              *  @ngInject
              */
             function ppIcheck($timeout) {
-                return {
+
+                var directive = {
                     restrict: 'A',
                     require: 'ngModel',
-                    link: function ($scope, element, $attrs, ngModel) {
-                        return $timeout(function () {
-                            var value;
-                            value = $attrs.value;
-
-                            $scope.$watch($attrs.ngModel, function (newValue) {
-                                $(element).iCheck('update');
-                            });
-
-                            return $(element).iCheck({
-                                checkboxClass: 'icheckbox_square-green',
-                                radioClass: 'iradio_square-green'
-
-                            }).on('ifChanged', function (event) {
-                                if ($(element).attr('type') === 'checkbox' && $attrs.ngModel) {
-                                    $scope.$apply(function () {
-                                        return ngModel.$setViewValue(event.target.checked);
-                                    });
-                                }
-                                if ($(element).attr('type') === 'radio' && $attrs.ngModel) {
-                                    return $scope.$apply(function () {
-                                        return ngModel.$setViewValue(value);
-                                    });
-                                }
-                            });
-                        });
-                    }
-
+                    link: linkFunc
                 };
-            }
 
+                return directive;
+
+                /**
+                 *  @ngInject
+                 */
+                function linkFunc($scope, element, $attrs, ngModel) {
+
+                    return $timeout(timeoutHandler);
+
+                    function timeoutHandler() {
+
+                        var value = $attrs.value;
+                        var icheckConfig = {checkboxClass: 'icheckbox_square-green', radioClass: 'iradio_square-green'};
+
+                        $scope.$watch($attrs.ngModel, ngModelWatch);
+
+                        return $(element).iCheck(icheckConfig).on('ifChanged', ifChangedHandle);
+
+
+                        function ngModelWatch(newValue) {
+                            $(element).iCheck('update');
+                        }
+
+                        function ifChangedHandle(event) {
+                            if ($(element).attr('type') === 'checkbox' && $attrs.ngModel) {
+                                $scope.$apply(function () {
+                                    return ngModel.$setViewValue(event.target.checked);
+                                });
+                            }
+                            if ($(element).attr('type') === 'radio' && $attrs.ngModel) {
+                                return $scope.$apply(function () {
+                                    return ngModel.$setViewValue(value);
+                                });
+                            }
+                        }
+                    }
+                }
+            }
 })();
