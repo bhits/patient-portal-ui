4252b70aee59a9c341233a266b549aae8c650737
tomson ngassa <tomson.ngassa@feisystems.com>
2016-01-05 15:51:23 -0500

    Include omitted health information controller and config
    #7692 - As a patient, I can view my health information, based on the latest transformed JSON from IExHub

--- client/app/healthinformation/controllers/healthInformation.controller.js
+++ client/app/healthinformation/controllers/healthInformation.controller.js
@@ -3,107 +3,34 @@
 
     'use strict';
 
-    /**
-     * The health Information Cotnroller
-     *
-     * @param $scope - The AngulularJS scope service.
-     * @param $stateParams - The state parameter service.
-     * @param $state - The state srevice.
-     * @param utilityService - The application utility service.
-     * @param patientData - The health information resolved data from the backend
-     * @param HealthInformationService - The health information service.
-     * @param $rootScope - The Angularjs root scope service.
-     *
-     */
-    function HealthInformationController($scope, $stateParams, $state, utilityService, patientData, HealthInformationService, $rootScope){
-        var HealthInformationVm = this;
+    angular
+        .module("app.healthInformation")
+            .controller('HealthInformationController', HealthInformationController);
 
-        $scope.showHealthInformationMenu = utilityService.getShowHealthInformationMenu();
+            /* @ngInject */
+            function HealthInformationController($rootScope, $scope, utilityService, patientData, healthInformationService ){
+                var vm = this;
 
-        HealthInformationVm.toggleMenu = false;
+                $scope.showHealthInformationMenu = utilityService.getShowHealthInformationMenu();
 
-        // Process the resolved patient CCDA data coming from the backend
+                vm.toggleMenu = false;
 
-        HealthInformationVm.healthinfotmation = HealthInformationService.getHealthInformation(patientData[0]);
-        HealthInformationVm.document =  HealthInformationService.getSectionByName(HealthInformationVm.healthinfotmation, 'Document');
-        HealthInformationVm.provider =  HealthInformationService.getSectionByName(HealthInformationVm.document, 'author');
-        HealthInformationVm.patient =  HealthInformationService.getSectionByName(HealthInformationVm.document, 'patient');
-        HealthInformationVm.sections =  HealthInformationService.getSectionByName(HealthInformationVm.document, 'section');
-        HealthInformationVm.ccdheader =  HealthInformationService.getSectionByName(HealthInformationVm.healthinfotmation, 'CCDAHeader');
-        HealthInformationVm.patientDemographics =  HealthInformationService.getSectionByName(HealthInformationVm.ccdheader, 'DemographicInformation');
+                vm.healthinfotmation = healthInformationService.getHealthInformation(patientData[0]);
+                vm.document =  healthInformationService.getSectionByName(vm.healthinfotmation, 'Document');
+                vm.provider =  healthInformationService.getSectionByName(vm.document, 'author');
+                vm.patient =  healthInformationService.getSectionByName(vm.document, 'patient');
+                vm.sections =  healthInformationService.getSectionByName(vm.document, 'section');
+                vm.ccdheader =  healthInformationService.getSectionByName(vm.healthinfotmation, 'CCDAHeader');
+                vm.patientDemographics =  healthInformationService.getSectionByName(vm.ccdheader, 'DemographicInformation');
 
-        /**
-         *  Expands all the accordion
-         */
-        HealthInformationVm.expandAllAccordions = function(){
-            HealthInformationVm.noPatientDataAlert = false;
-            $rootScope.$broadcast('ExpandAccordions', { expand:true });
-        };
+                vm.expandAllAccordions = function(){
+                    vm.noPatientDataAlert = false;
+                    $rootScope.$broadcast('ExpandAccordions', { expand:true });
+                };
 
-        /**
-         * Collapse all the accordion
-         */
-        HealthInformationVm.collapseAllAccordions = function(){
-            HealthInformationVm.noPatientDataAlert = false;
-            $rootScope.$broadcast('CollapseAccordions', { collapse:true });
-        };
-    }
-
-    /**
-     * Resovlve the patient health information from the PHR using the Health Information Service
-     *
-     */
-    HealthInformationController.resolve = {
-        patientData: ['HealthInformationService', '$q', '$log','utilityService',  function(HealthInformationService, $q, $log, utilityService){
-
-            var deferred = $q.defer();
-
-            var healthInformationResource = HealthInformationService.getHealthInformationResource();
-            var healthInformationData = healthInformationResource.get(
-                {mrn: '2323'},
-                function(response){ return response;},
-                function(response){ return response ;});
-
-            $q.all([healthInformationData.$promise]).then(function(response) {
-                deferred.resolve(response);
-            });
-            return deferred.promise;
-        }]
-    };
-
-    /**
-     * The health Information Config function
-     *
-     * @param $stateProvider
-     */
-    function Config($stateProvider){
-        $stateProvider
-            .state('fe.patient', {
-                abstract: true,
-                url: '/patient',
-                templateUrl: 'app/layout/content.html'
-            })
-            .state('fe.patient.healthinformation', {
-                url: '/healthinformation',
-                params: { scrollTo : null, expand: null},
-                data: { pageTitle: 'Health Information' },
-                templateUrl: 'app/healthinformation/tmpl/healthInformation.html',
-                controller: 'HealthInformationController',
-                controllerAs: 'HealthInformationVm',
-                resolve: HealthInformationController.resolve
-            });
-    }
-
-
-    /**
-     *  The health Information mmodule
-     */
-    angular.module("app.healthInformationModule",
-        [
-            'app.core',
-            'app.healthInformationService',
-            'app.healthInformationDirectivesModule'
-        ])
-        .config(Config)
-        .controller('HealthInformationController', HealthInformationController);
+                vm.collapseAllAccordions = function(){
+                    vm.noPatientDataAlert = false;
+                    $rootScope.$broadcast('CollapseAccordions', { collapse:true });
+                };
+            }
 })();
