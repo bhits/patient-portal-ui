821652483cab962fa44c7e2192a619776b5e8481
JiahaoLi <Jiahao.Li@feisystems.com>
2016-03-02 23:22:26 -0500

    Create oauthAuthentication Service
    #8196 - As a patient, when I login to the MHC, the security protocols automatically authorize access to MHC apps

---
+++ client/app/security/services/oauthAuthentication.service.js
@@ -0,0 +1,80 @@
+(function () {
+    'use strict';
+
+    angular.module('app.security')
+        .factory('oauthAuthenticationService', OauthAuthenticationService);
+
+    /* @ngInject */
+    function OauthAuthenticationService($http, tokenService, oauthConfig) {
+
+        var service = {};
+
+        service.allowAnonymous = allowAnonymous;
+        service.login = login;
+        service.refresh = refresh;
+        service.logout = logout;
+
+        return service;
+
+        function setTokenValues(response) {
+            tokenService.setToken(response.access_token); //jshint ignore:line
+            tokenService.setExpiresIn(response.expires_in);// jshint ignore:line
+            tokenService.setRefreshToken(response.refresh_token);// jshint ignore:line
+        }
+
+        function allowAnonymous(targetPath) {
+            if (targetPath.split('/').length > 2) {
+                targetPath = '/' + targetPath.split('/')[1];
+            } else if (targetPath.split('?').length > 1) {
+                targetPath = targetPath.split('?')[0];
+            }
+
+            return oauthConfig.forgotPasswordURL === targetPath || oauthConfig.unsecuredPaths.indexOf(targetPath) > -1;
+        }
+
+        function login(username, password) {
+            return $http({
+                method: 'POST',
+                url: oauthConfig.getAccessTokenUrl,
+                headers: {'Authorization': 'Basic ' + oauthConfig.base64BasicKey},
+                params: {
+                    'grant_type': 'password',
+                    'password': password,
+                    'username': username
+                }
+            })
+                .success(function (response) {
+                    setTokenValues(response);
+                })
+                .error(function () {
+                    tokenService.reset();
+                });
+        }
+
+        function refresh() {
+            return $http({
+                method: 'POST',
+                url: oauthConfig.getAccessTokenUrl,
+                headers: {'Authorization': 'Basic ' + oauthConfig.base64BasicKey},
+                params: {
+                    'refresh_token': tokenService.getRefreshToken(),
+                    'grant_type': 'refresh_token'
+                }
+            })
+                .success(function (response) {
+                    setTokenValues(response);
+                });
+        }
+
+        function logout() {
+            return $http({
+                method: 'DELETE',
+                url: oauthConfig.revokeTokenUrl,
+                headers: {'Authorization': 'Bearer ' + tokenService.getToken()}
+            })
+                .success(function () {
+                    tokenService.reset();
+                });
+        }
+    }
+})();
