cc56e6f5535be21268222d9f336a501320dc0288
JiahaoLi <Jiahao.Li@feisystems.com>
2016-03-02 23:28:17 -0500

    Configure oauthConfig in security config
    #8196 - As a patient, when I login to the MHC, the security protocols automatically authorize access to MHC apps

--- client/app/security/security.config.js
+++ client/app/security/security.config.js
@@ -9,19 +9,50 @@
 
     angular
         .module('app.security')
-            .config(SecurityConfig);
+        .constant('oauthConfig', {
+            getAccessTokenUrl: '',
+            base64BasicKey: '',
+            revokeTokenUrl: '',
+            loginPath: '/login',
+            loginSuccessPath: '',
+            interceptorIgnorePattern: / /,
+            loginErrorMessage: '',
+            loginFunction: null,
+            forgotPasswordURL: null,
+            logoutSuccessMessage: '',
+            storageType: 'session',
+            useRouting: true,
+            unsecuredPaths: []
+        })
+        .config(SecurityConfig);
 
-            /* @ngInject */
-            function SecurityConfig($stateProvider){
+    /* @ngInject */
+    function SecurityConfig($stateProvider, oauthConfig) {
 
-                $stateProvider
-                    .state('fe.login', {
-                        url: "/login",
-                        data: {pageTitle: 'Login'},
-                        templateUrl: "app/security/securityLogin.html",
-                        controllerAs: "loginVm",
-                        controller: 'LoginController'
-                    });
-            }
+        $stateProvider
+            .state('fe.login', {
+                url: "/login",
+                data: {pageTitle: 'Login'},
+                templateUrl: "app/security/securityLogin.html",
+                controllerAs: "loginVm",
+                controller: 'LoginController'
+            });
+
+        oauthConfig.getAccessTokenUrl = 'https://localhost:8443/uaa/oauth/token';
+
+        /*clientSecret*/
+        oauthConfig.base64BasicKey = 'cGF0aWVudC1wb3J0YWwtdWk6Y2hhbmdlaXQ=';
+
+        //oauthConfig.revokeTokenUrl = 'http://www.mysite.com/token';
+        oauthConfig.loginSuccessPath = '/fe/index/home';
+        oauthConfig.interceptorIgnorePattern = /oauth\/token/;
+        oauthConfig.loginErrorMessage = 'error_description';
+        oauthConfig.logoutSuccessMessage = 'logoutSuccess';
+        oauthConfig.storageType = 'session';
+        oauthConfig.useRouting = true;
+        //oauthConfig.forgotPasswordURL = 'http://localhost/#/forgot-password';
+        //oauthConfig.unsecuredPaths.push('/forgot-password');
+        //oauthConfig.unsecuredPaths.push('/unsecured');
+    }
 
 })();
