3b0a11440f72355f8a8391c4af4478dfcce02643
JiahaoLi <Jiahao.Li@feisystems.com>
2016-03-18 14:39:46 -0400

    Create account verifcation directive
    #8416 - As a patient, I can activate my account to login and access MHC

---
+++ client/app/account/directives/mhcAccountVerification.directive.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Jiahao.Li on 3/18/2016.
+ */
+
+(function () {
+    'use strict';
+
+    angular
+        .module('app.account')
+        .directive('mhcAccountVerification', mhcAccountVerification);
+
+    function mhcAccountVerification() {
+
+        var directive = {
+            restrict: 'E',
+            replace: true,
+            templateUrl: 'app/account/directives/verification.html',
+            scope: {},
+            bindToController: {},
+            controller: VerificationController,
+            controllerAs: 'verificationVm'
+        };
+
+        return directive;
+
+        /* @ngInject */
+        function VerificationController() {
+            var vm = this;
+            vm.clearField = clearField;
+            var verificationFormMaster = {month: "", day: "", year: "", verificationCode: ""};
+
+            function clearField(verificationForm) {
+                if (verificationForm) {
+                    verificationForm.$setPristine();
+                    verificationForm.$setUntouched();
+                    vm.verificationFormMaster = angular.copy(verificationFormMaster);
+                }
+            }
+        }
+    }
+})();
