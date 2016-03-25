5f412cce1c5df49fb25c6041ace6baa9190a61a8
JiahaoLi <Jiahao.Li@feisystems.com>
2016-03-24 22:52:35 -0400

    Create validation directive for checking password
    #8416 - As a patient, I can activate my account to login and access MHC

---
+++ client/app/core/core.config.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Jiahao.Li on 3/24/2016.
+ */
+
+(function () {
+
+    'use strict';
+
+    angular.module('app.core')
+        .constant("coreConstants",
+            {
+                PASSWORD_REGEX: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/
+            });
+})();
