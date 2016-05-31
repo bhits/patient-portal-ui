e1d819254750e6f9b4a93f792f30c859fe434584
TomsonNgassa <Tomson.Ngassa@feisystems.com>
2016-05-30 21:17:37 -0400

    Fix the authentication issue in the consent attestation.
    #8795 - As a patient, for signing a consent, I will be able to use a checkbox attestation feature

--- client/app/consent/directives/ppConsentESignature.directive.js
+++ client/app/consent/directives/ppConsentESignature.directive.js
@@ -53,30 +53,30 @@
                     }
                 }
 
-                vm.patient = {
-                    createdDate: "04/08/2016",
-                    dob: "04/08/2016",
-                    name: "Bob Lastname",
-                    consentReferenceNumber: "MHC.HC3AFH:&2.16.840.1.113883.3.704.100.200.1.1.3.1&ISO:1518239631:1790718047:OEATAM",
-                    authrizeProviderName: "KOYOMJI DENTAL",
-                    authorizeProviderNPI: "1992916092",
-                    authorizeProviderAddress: "8218 WISCONSIN AVE,BETHESDA, MD, 20814-3107",
-                    authorizeProviderTelephone:"301-654-1111",
-                    discloseProviderName:"SABA PEDIATRIC MEDICINE, LLC",
-                    discloseProviderNPI: "1871783456",
-                    discloseProviderAddress: "15225 SHADY GROVE RD,ROCKVILLE, MD, 20850-3258",
-                    discloseProviderTelephone:"301-838-8977",
-                    sensitiveCategories: [
-                        {displayName: "Addictions Information"},
-                        {displayName:"Alcohol use and Alcoholism Information"}
-                    ],
-                    purposeOfUses: [
-                        {displayName: "Health Treatment"}
-                    ],
-                    effectiveDate: "04/08/2016",
-                    expirationDate: "04/08/2017",
-                    acceptTerms:false
-                };
+                // vm.patient = {
+                //     createdDate: "04/08/2016",
+                //     dob: "04/08/2016",
+                //     name: "Bob Lastname",
+                //     consentReferenceNumber: "MHC.HC3AFH:&2.16.840.1.113883.3.704.100.200.1.1.3.1&ISO:1518239631:1790718047:OEATAM",
+                //     authrizeProviderName: "KOYOMJI DENTAL",
+                //     authorizeProviderNPI: "1992916092",
+                //     authorizeProviderAddress: "8218 WISCONSIN AVE,BETHESDA, MD, 20814-3107",
+                //     authorizeProviderTelephone:"301-654-1111",
+                //     discloseProviderName:"SABA PEDIATRIC MEDICINE, LLC",
+                //     discloseProviderNPI: "1871783456",
+                //     discloseProviderAddress: "15225 SHADY GROVE RD,ROCKVILLE, MD, 20850-3258",
+                //     discloseProviderTelephone:"301-838-8977",
+                //     sensitiveCategories: [
+                //         {displayName: "Addictions Information"},
+                //         {displayName:"Alcohol use and Alcoholism Information"}
+                //     ],
+                //     purposeOfUses: [
+                //         {displayName: "Health Treatment"}
+                //     ],
+                //     effectiveDate: "04/08/2016",
+                //     expirationDate: "04/08/2017",
+                //     acceptTerms:false
+                // };
 
                 function onchecked(){
                     if(vm.patient.acceptTerms){
@@ -112,18 +112,24 @@
                     }
                     
                     function authenticate(){
-                        authenticationService.login(profileService.getUserName(), authenticateVm.password)
-                            .then(
-                                function (response) {
-                                    authenticateVm.errorMessage="";
-                                    vm.isAuthenticated = true;
-                                    notificationService.success("Success in authenticating patient.");
-                                    $modalInstance.close();
-                                }, function (error) {
-                                    vm.isAuthenticated = false;
-                                    authenticateVm.errorMessage="Invalid password.";
-                                }
-                            );
+                        var loginInfo = {
+                            grant_type: 'password',
+                            response_type: 'token',
+                            username: profileService.getUserName(),
+                            password: authenticateVm.password
+                        };
+                        var success = function (response) {
+                            authenticateVm.errorMessage="";
+                            vm.isAuthenticated = true;
+                            notificationService.success("Success in authenticating patient.");
+                            $modalInstance.close();
+                        };
+                        var error = function (error) {
+                            vm.isAuthenticated = false;
+                            authenticateVm.errorMessage="Invalid password.";
+                        };
+
+                        authenticationService.login(loginInfo, success, error);
                     }
                 }
             }
