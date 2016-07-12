03207e60b8663c2d3b50fc1b8ac0469e14d99621
Cindy Ren <cindy.ren@feisystems.com>
2016-07-12 17:10:08 -0400

    Changed controllers to correct getBrandName method and created tests for new controllers

--- client/app/account/controllers/login.controller.spec.js
+++ client/app/account/controllers/login.controller.spec.js
@@ -12,24 +12,58 @@ describe("app.login.controller ", function() {
     beforeEach(module('app.security'));
     beforeEach(module('app.brand'));
 
-    var envService, controller, $resource;
+    var envService, controller, $resource, brand, utilityService, accountConfig;
 
-    beforeEach(inject(function( $controller, $state, _Idle_, _authenticationService_,
-                                _envService_, _utilityService_, _$resource_, _brand_) {
+    beforeEach(function () {
+        var fakeModule = angular.module('test.app.brand', function () {});
+        fakeModule.config( function (brandProvider) {
+            brand = brandProvider;
+        });
+        module('app.brand', 'test.app.brand');
+        inject(function () {});
+        brand.setBrandName("Brand Name");
+        brand.setBrandInitial("BI");
+    });
+
+    beforeEach(inject(function( $controller, $state, _envService_,
+                                _utilityService_, _$resource_, _accountConfig_) {
 
         envService = _envService_;
         $resource = _$resource_;
+        utilityService = _utilityService_;
+        accountConfig = _accountConfig_;
 
         controller = $controller('LoginController', {
             envService: envService,
-            brand: _brand_
+            utilityService: utilityService,
+            accountConfig: accountConfig,
+            brand: brand
         });
+
     }));
 
     it('should create controller and have correct version based on config.js', function(){
         expect(controller).toBeDefined();
         expect(controller.version).toBeDefined();
         expect(controller.version).toEqual('0.12.0');
+    });
+
+    it('should forget password', function(){
+        expect(controller.forgotPassword).toBeDefined();
+        spyOn(utilityService, 'redirectTo').and.callThrough();
+        controller.forgotPassword();
+        expect(accountConfig.forgotPasswordPath).toBe('/fe/account/forgotPassword');
+        expect(utilityService.redirectTo).toHaveBeenCalledWith('/fe/account/forgotPassword');
+    });
+
+    it('should get brand name', function(){
+        expect(controller.brandName).toBeDefined();
+        expect(controller.brandName).toEqual('Brand Name');
+    });
+
+    it('should get brand initials for alt logo text', function(){
+        expect(controller.altLogoText).toBeDefined();
+        expect(controller.altLogoText).toEqual('BI Logo');
     });
 
 });
---
+++ client/app/account/controllers/resetPassword.controller.spec.js
@@ -0,0 +1,44 @@
+/**
+ * Created by cindy.ren on 7/12/2016.
+ */
+
+'use strict';
+
+describe("app.activateController", function() {
+
+    beforeEach(module('app.account'));
+
+    var controller, brand;
+
+
+    beforeEach(function () {
+        var fakeModule = angular.module('test.app.brand', function () {});
+        fakeModule.config( function (brandProvider) {
+            brand = brandProvider;
+        });
+        module('app.brand', 'test.app.brand');
+        inject(function () {});
+        brand.setBrandName("Brand Name");
+        brand.setBrandInitial("BI");
+    });
+
+    beforeEach(inject(function( $controller, _$state_, _accountService_) {
+
+
+        controller = $controller('ResetPasswordController', {
+            brand: brand
+        });
+
+    }));
+
+    it('should create controller ', function(){
+        expect(controller).toBeDefined();
+    });
+
+    it('should have correct title', function(){
+        expect(controller.title).toBeDefined();
+        expect(controller.title).toEqual('Brand Name Reset Password Complete');
+    });
+    
+
+});
