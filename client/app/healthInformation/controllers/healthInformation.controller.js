32cabf2ff2b92c395dd71cc53134b5b2f4033e81
JiahaoLi <Jiahao.Li@feisystems.com>
2016-07-22 16:26:52 -0400

    Implement to dynamically get patient health information
    #9931 - As a developer, I will have enhanced the PHR code in order to display patient health information

--- client/app/healthInformation/controllers/healthInformation.controller.js
+++ client/app/healthInformation/controllers/healthInformation.controller.js
@@ -1,26 +1,36 @@
-﻿
-(function () {
+﻿(function () {
 
     'use strict';
 
     angular
         .module("app.healthInformation")
-            .controller('HealthInformationController', HealthInformationController);
+        .controller('HealthInformationController', HealthInformationController);
 
-            /* @ngInject */
-            function HealthInformationController($rootScope, patientData, healthInformationService ){
-                var vm = this;
+    /* @ngInject */
+    function HealthInformationController($rootScope, patientData) {
+        var vm = this;
 
-                vm.documents = healthInformationService.getDocuments(patientData);
+        vm.documents = getPatientDocument(patientData);
 
-                vm.expandAllAccordions = function(){
-                    vm.noPatientDataAlert = false;
-                    $rootScope.$broadcast('ExpandAccordions', { expand:true });
-                };
+        vm.expandAllAccordions = function () {
+            vm.noPatientDataAlert = false;
+            $rootScope.$broadcast('ExpandAccordions', {expand: true});
+        };
 
-                vm.collapseAllAccordions = function(){
-                    vm.noPatientDataAlert = false;
-                    $rootScope.$broadcast('CollapseAccordions', { collapse:true });
-                };
+        vm.collapseAllAccordions = function () {
+            vm.noPatientDataAlert = false;
+            $rootScope.$broadcast('CollapseAccordions', {collapse: true});
+        };
+
+        function getPatientDocument(data) {
+            if (angular.isDefined(data)) {
+                var patientDocument = data.Documents;
+                if (angular.isDefined(patientDocument) && patientDocument.length > 0) {
+                    return patientDocument;
+                } else {
+                    vm.noDocumentFound = true;
+                }
             }
+        }
+    }
 })();
--- client/app/healthInformation/healthInformation.config.js
+++ client/app/healthInformation/healthInformation.config.js
@@ -6,41 +6,47 @@
 
     angular
         .module('app.healthInformation')
-            .config(healthInforamationConfig);
+        .config(healthInforamationConfig);
 
-            /* @ngInject */
-            function healthInforamationConfig($stateProvider){
-                $stateProvider
-                    .state('fe.patient', {
-                        abstract: true,
-                        url: '/patient',
-                        templateUrl: 'app/layout/content.html'
-                    })
-                    .state('fe.patient.healthinformation', {
-                        url: '/healthinformation',
-                        params: { scrollTo : null, expand: null},
-                        data: { pageTitle: 'Health Information' },
-                        templateUrl: 'app/healthInformation/controllers/healthInformation.html',
-                        controller: 'HealthInformationController',
-                        controllerAs: 'healthInformationVm',
-                        resolve: {
-                            /* @ngInject */
-                            patientData: ['healthInformationService', '$q',  function(healthInformationService, $q){
+    /* @ngInject */
+    function healthInforamationConfig($stateProvider) {
+        $stateProvider
+            .state('fe.patient', {
+                abstract: true,
+                url: '/patient',
+                templateUrl: 'app/layout/content.html'
+            })
+            .state('fe.patient.healthinformation', {
+                url: '/healthinformation',
+                params: {scrollTo: null, expand: null},
+                data: {pageTitle: 'Health Information'},
+                templateUrl: 'app/healthInformation/controllers/healthInformation.html',
+                controller: 'HealthInformationController',
+                controllerAs: 'healthInformationVm',
+                resolve: {
+                    /* @ngInject */
+                    patientData: function ($q, healthInformationService, notificationService) {
+                        var deferred = $q.defer();
 
-                                var deferred = $q.defer();
+                        var success = function (response) {
+                            return response;
+                        };
 
-                                var healthInformationResource = healthInformationService.getHealthInformationResource();
-                                var healthInformationData = healthInformationResource.get(
-                                    {mrn: '2323'},
-                                    function(response){ return response;},
-                                    function(response){ return response ;});
+                        var error = function (data) {
+                            notificationService.error('Failed to get the patient health information, please try again later...');
+                            return data;
+                        };
 
-                                $q.all([healthInformationData.$promise]).then(function(response) {
-                                    deferred.resolve(response);
-                                });
-                                return deferred.promise;
-                            }]
-                        }
-                    });
-            }
+                        var healthInformationPromise = healthInformationService.getHealthInformation(success, error).$promise;
+                        healthInformationPromise.then(function (response) {
+                            deferred.resolve(response);
+                        }, function (error) {
+                            deferred.reject(error);
+                        });
+
+                        return deferred.promise;
+                    }
+                }
+            });
+    }
 })();
