15c101194d011c5383a9d51260742898904d13c1
JiahaoLi <Jiahao.Li@feisystems.com>
2016-09-06 13:58:26 -0400

    Change pp prefix to c2s in IboxTools directive
    #10703 - As a developer, I will complete rebranding changes in code (front-end code)

---
+++ client/app/layout/directives/c2sIboxTools.directive.js
@@ -0,0 +1,102 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.layout')
+        .directive('c2sIboxTools', c2sIboxTools);
+
+    /* @ngInject */
+    function c2sIboxTools() {
+        var directive = {
+            restrict: 'A',
+            scope: {},
+            bindToController: {
+                total: '@',
+                ppCollapsed: '=?',
+                ppChevronPreventDefault: '=?'
+            },
+            templateUrl: 'app/layout/directives/iBoxTools.html',
+            controller: IboxToolController,
+            controllerAs: 'iboxToolVm'
+
+        };
+        return directive;
+    }
+
+    /* @ngInject */
+    function IboxToolController($scope, $element, $timeout) {
+        var iboxToolVm = this;
+        iboxToolVm.ppChevronPreventDefault = !!iboxToolVm.ppChevronPreventDefault;
+        var ibox = $element.closest('div.ibox');
+        var icon = $element.find('i:first');
+        var content = ibox.find('div.ibox-content');
+        iboxToolVm.toggleCollapsed = toggleCollapsed;
+
+        $scope.$on('ExpandAccordions', ExpandAccordionsEventHandler);
+        $scope.$on('CollapseAccordions', CollapseAccordionsEventHandler);
+
+        $scope.$watch('iboxToolVm.ppCollapsed', collapseWatchHandler);
+
+        activate();
+
+        function activate() {
+            if (iboxToolVm.ppCollapsed) {
+                content.slideUp(0);
+            }
+        }
+
+        function toggleCollapsed() {
+            if (!iboxToolVm.ppChevronPreventDefault) {
+                iboxToolVm.ppCollapsed = !iboxToolVm.ppCollapsed;
+            }
+        }
+
+        function ExpandAccordionsEventHandler(event, args) {
+
+            if (args.expand) {
+                //Accordion down
+                content.slideDown(200);
+
+                if (icon.hasClass('fa-chevron-down')) {
+                    icon.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+                }
+                ibox.toggleClass('');
+                resize();
+            }
+        }
+
+        function CollapseAccordionsEventHandler(event, args) {
+
+            if (args.collapse) {
+                //Accordion down
+                content.slideUp(200);
+
+                if (icon.hasClass('fa-chevron-up')) {
+                    icon.toggleClass('fa-chevron-down').toggleClass('fa-chevron-up');
+                }
+                ibox.toggleClass('');
+                resize();
+            }
+        }
+
+        function collapseWatchHandler(newValue, oldValue) {
+            if (newValue !== oldValue) {
+                showhide();
+            }
+        }
+
+        function showhide() {
+            content.slideToggle(200);
+            // Toggle icon from up to down
+            icon.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+            resize();
+        }
+
+        function resize() {
+            $timeout(function () {
+                ibox.resize();
+                ibox.find('[id^=map-]').resize();
+            }, 50);
+        }
+    }
+})();
