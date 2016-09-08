689876214f3a9b311e4e1c622c5a5ce35d6cdbc2
JiahaoLi <Jiahao.Li@feisystems.com>
2016-09-07 20:49:42 -0400

    Change pp prefix to c2s in ConsentModalHeader directive
    #10703 - As a developer, I will complete rebranding changes in code (front-end code)

---
+++ client/app/consent/directives/c2sConsentModalHeader.directives.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.consent')
+        .directive('c2sConsentModalHeader', consenModalHeader);
+
+    function consenModalHeader() {
+
+        var directive = {
+            restrict: 'E',
+            replace: false,
+            templateUrl: 'app/consent/directives/consentModalHeader.html',
+            scope: {},
+            bindToController: {
+                option: '=',
+                cancel: '&'
+            },
+            controllerAs: 'consentModalHeaderVm',
+            controller: ConsentModalHeaderController
+        };
+        return directive;
+
+        function ConsentModalHeaderController() {
+            var vm = this;
+        }
+    }
+})();
