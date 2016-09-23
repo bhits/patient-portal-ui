706160a48795270692d356b4c9d107c715eb3cb0
JiahaoLi <Jiahao.Li@feisystems.com>
2016-09-22 20:56:00 -0400

    Format and clean up home module
    #10926 - As a developer, I will have prepared admin-portal-ui,  patient-portal-ui code to make it more deployable by community member (HIE)

--- client/app/home/home.controller.spec.js
+++ client/app/home/home.controller.spec.js
@@ -1,37 +1,38 @@
 /**
  * Created by cindy.ren on 7/13/2016.
  */
-
 'use strict';
 
-xdescribe("app.HomeController", function() {
+xdescribe("app.HomeController", function () {
 
     beforeEach(module('app.home'));
 
     var controller, brand;
 
     beforeEach(function () {
-        var fakeModule = angular.module('test.app.brand', function () {});
-        fakeModule.config( function (brandProvider) {
+        var fakeModule = angular.module('test.app.brand', function () {
+        });
+        fakeModule.config(function (brandProvider) {
             brand = brandProvider;
         });
         module('app.brand', 'test.app.brand');
-        inject(function () {});
+        inject(function () {
+        });
         brand.setBrandName("Brand Name");
     });
 
-    beforeEach(inject(function( $controller) {
+    beforeEach(inject(function ($controller) {
 
         controller = $controller('HomeController', {
             brand: brand
         });
 
     }));
-    it('should create controller ', function(){
+    it('should create controller ', function () {
         expect(controller).toBeDefined();
     });
 
-    it('should set correct data', function(){
+    it('should set correct data', function () {
         expect(controller.brandName).toBeDefined();
         expect(controller.brandName).toEqual("Brand Name");
     });
