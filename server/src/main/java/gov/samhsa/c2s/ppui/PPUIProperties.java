3c32994c48b235829bead6bc97528b62a04a3286
burcakulug <burcak.ulug@feisystems.com>
2016-11-03 11:10:51 -0400

    Serve UI configuration from runtime properties
    #11661:As a developer, I will have upgrade UI projects to get configuration at run time

---
+++ server/src/main/java/gov/samhsa/c2s/ppui/PPUIProperties.java
@@ -0,0 +1,52 @@
+package gov.samhsa.c2s.ppui;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+
+@Component
+@ConfigurationProperties(prefix = "c2s.pp-ui")
+@Data
+public class PPUIProperties {
+
+    private Oauth2 oauth2;
+    private SecuredApis securedApis;
+    private UnsecuredApis unsecuredApis;
+
+    @Data
+    public static class Oauth2 {
+        private Client client;
+
+        @Data
+        public static class Client {
+            @JsonIgnore
+            private String clientId;
+            @JsonIgnore
+            private String secret;
+
+            public byte[] getBase64BasicKey() {
+                return (clientId + ":" + secret).getBytes(StandardCharsets.UTF_8);
+            }
+        }
+    }
+
+    @Data
+    public static class SecuredApis {
+        private String pcmApiBaseUrl;
+        private String phrApiBaseUrl;
+        private String tryPolicyApiBaseUrl;
+        private String userInfo;
+    }
+
+    @Data
+    public static class UnsecuredApis {
+        private String plsApiBaseUrl;
+        private String tokenUrl;
+        private String verificationUrl;
+        private String activationUrl;
+        private String forgotPasswordUrl;
+    }
+}
