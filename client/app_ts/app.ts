b79f652e81d6152a5ae24db6ea5562faa767493e
JiahaoLi <Jiahao.Li@feisystems.com>
2016-12-14 10:18:07 -0500

    Add angular 2 root module
    #12133 - As a developer, I will have implemented Angular 2 with Typescript in the PP–UI

---
+++ client/app_ts/app.ts
@@ -0,0 +1,36 @@
+import {
+    NgModule,
+    forwardRef,
+} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {
+    FormsModule,
+} from '@angular/forms';
+import {BrowserModule} from "@angular/platform-browser";
+import {UpgradeAdapter} from '@angular/upgrade';
+import {HttpModule} from "@angular/http";
+
+
+/*
+ * Create our upgradeAdapter
+ */
+const upgradeAdapter: UpgradeAdapter = new UpgradeAdapter(
+    forwardRef(() => PPUIModule));
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule,
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: []
+})
+class PPUIModule {
+}
+
+/*
+ * Bootstrap the App
+ */
+upgradeAdapter.bootstrap(document.body, ['bootstrapApp']);
