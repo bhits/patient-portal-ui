81670043aafabb7ec37ced23570671e78281c0ba
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-01-12 15:22:50 -0500

    Fix unit test to work with the new projection provider JSON
    #12362 - As a developer, I will have Integrate the new PLS API in C2S , In order to support feature for multi-provider adding

--- client/app/provider/filters/plsAddress.filter.js
+++ client/app/provider/filters/plsAddress.filter.js
@@ -14,9 +14,9 @@
         return function (provider) {
             var providerAddressArray = [provider.firstLineBusinessPracticeLocationAddress,
                 provider.secondLineBusinessPracticeLocationAddress,
-                provider.businessPracticeLocationAddressCityName,
-                provider.businessPracticeLocationAddressStateName,
-                utilityService.formatZipCode(provider.businessPracticeLocationAddressPostalCode)].filter(function (element) {
+                provider.practiceLocationAddressCityName,
+                provider.practiceLocationAddressStateName,
+                utilityService.formatZipCode(provider.practiceLocationAddressPostalCode)].filter(function (element) {
                 return !!utilityService.hasString(element);
             });
             return providerAddressArray.join(", ");
