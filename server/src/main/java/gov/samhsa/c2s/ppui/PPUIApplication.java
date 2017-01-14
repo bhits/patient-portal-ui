7dc5aa596dc6c1d848eaccb967f7a26511a0f67d
JiahaoLi <Jiahao.Li@feisystems.com>
2017-01-14 14:14:16 -0500

    Enable security scope for accessing management endpoint
    #12361 - As a developer, I can use a centralized configuration management tool

--- server/src/main/java/gov/samhsa/c2s/ppui/PPUIApplication.java
+++ server/src/main/java/gov/samhsa/c2s/ppui/PPUIApplication.java
@@ -3,12 +3,14 @@ package gov.samhsa.c2s.ppui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @Controller
 @EnableDiscoveryClient
+@EnableResourceServer
 public class PPUIApplication {
 
     @RequestMapping(value = "/fe/**")
@@ -19,5 +21,4 @@ public class PPUIApplication {
     public static void main(String[] args) {
         SpringApplication.run(PPUIApplication.class, args);
     }
-
 }
