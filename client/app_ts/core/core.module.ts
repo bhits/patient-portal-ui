bd174362d2bad05ff527b09248e688032c891b2f
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-01-26 16:17:06 -0500

    Move the logout component into the new core module
    #12552 - As a developer, I will have Implement frontend for multi-provider adding feature

---
+++ client/app_ts/core/core.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+
+import {LogoutComponent} from './logout.component';
+
+@NgModule({
+    declarations: [LogoutComponent],
+    imports: [
+    ],
+    providers: [],
+    exports:[LogoutComponent]
+})
+
+export class CoreModule {
+}
