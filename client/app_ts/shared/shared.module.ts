ab2a8628a2171fa3f2a95a369b6d473d555e2a82
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-02-06 23:31:47 -0500

    implement c2s modal component
    #12552 - As a developer, I will have Implement frontend for multi-provider adding feature

--- client/app_ts/shared/shared.module.ts
+++ client/app_ts/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {C2SModalComponent} from './C2SModal.component';
 
 @NgModule({
-    declarations: [],
-    imports: [
-    ],
-    providers: []
+    declarations: [C2SModalComponent],
+    imports: [CommonModule],
+    providers: [],
+    exports:[C2SModalComponent]
 })
 
 export class SharedModule {
